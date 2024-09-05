ceb3a9ed5f3bbec5fb56405c8b350c7c3d2822fc
MyDesktop <dugubi1004@gmail.com>
2024-09-05 10:59:26 +0900

    rewrite 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   basePath: '/app',
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/src/app/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
