db84ded653e56aa18b30ff9720172998628d227f
MyDesktop <dugubi1004@gmail.com>
2024-09-05 11:24:51 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 export default function Home() {
   return (
     // 홈화면입니다.
-    <main className="flex min-h-screen flex-col p-6">HOME 화면입니다 fuck you vercel</main>
+    <main className="flex min-h-screen flex-col p-6">
+      HOME 화면입니다 fuck you vercel shit bastard
+    </main>
   );
 }
