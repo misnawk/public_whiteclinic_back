b18c7300ff785fb6ee9fb50bc7f9b469ba255f43
MyDesktop <dugubi1004@gmail.com>
2024-09-05 11:23:26 +0900

    test again and again....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ import React from 'react';
 export default function Home() {
   return (
     // 홈화면입니다.
-    <main className="flex min-h-screen flex-col p-6">HOME 화면입니다 fuck you</main>
+    <main className="flex min-h-screen flex-col p-6">HOME 화면입니다 fuck you vercel</main>
   );
 }
