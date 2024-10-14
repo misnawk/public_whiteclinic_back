433b34ce02f657b742e5d4a75155b19e4fb0b994
MyDesktop <dugubi1004@gmail.com>
2024-10-14 18:19:33 +0900

    1014 이지훈 - setupSwagger modified

--- backend/src/util/setupSwagger.ts
+++ backend/src/util/setupSwagger.ts
@@ -16,5 +16,5 @@ export function setupSwagger(app: INestApplication) {
 
   const document = SwaggerModule.createDocument(app, options);
 
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('/api-docs', app, document);
 }
