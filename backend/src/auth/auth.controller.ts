e59f586c17793fc35dccf4fcf1ee0ecf3c0ed12a
MyDesktop <dugubi1004@gmail.com>
2024-10-23 21:28:37 +0900

    1023 이지훈 - 배포 테스팅

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiBearerAuth, ApiBody, ApiResponse } from '@nestjs/swagger';
 import { RegisterAuthDTO } from './dto/register-auth.dto';
