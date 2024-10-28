0a545e8d7b9367e1fe6a8a73465032d12af1ce2f
misnawk <kms13011@naver.com>
2024-10-28 17:40:20 +0900

    no message

--- backend/src/util/URLs.ts
+++ backend/src/util/URLs.ts
@@ -1,4 +1,3 @@
 export const SERVER_PORT = 8000;
 export const LOCAL_URL = 'http://localhost:8000';
-export const SERVER_URL =
-  'https://public-whiteclinic-back-904tkc8om-misnawks-projects.vercel.app';
+export const SERVER_URL = 'https://public-whiteclinic-back-rq7t.vercel.app/';
